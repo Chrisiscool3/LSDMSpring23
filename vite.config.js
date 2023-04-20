734ac8d284dd063b4e79799286382b33fe474ee9
Hassan Ali <hassan.ali@my.utsa.edu>
2023-04-20 12:33:48 -0500

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name].[hash][extname]",
-      },
-    },
-  },
 });
