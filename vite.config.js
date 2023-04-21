0336ca33fb512530f4ade69d0b6e227d62e27054
Hassan Ali <hassan.ali@my.utsa.edu>
2023-04-20 23:39:03 -0500

    modified client.js

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-      },
-    },
-    outDir: "build",
-    assetsDir: "assets",
-    // Copy the images folder to the build output
-    // so they are available at runtime
-    copy: [
-      {
-        from: "images",
-        to: "assets/images",
-      },
-    ],
-  },
 });
