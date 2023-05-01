88c0a43fbd5960f543c0ce4021789f036fe1d05b
Hassan Ali <hassan.ali@my.utsa.edu>
2023-05-01 08:40:08 -0500

    modified word cloud

--- src/pages/WordCloud.jsx
+++ src/pages/WordCloud.jsx
@@ -1,11 +1,29 @@
-import { VStack, Image } from "@chakra-ui/react";
+import { VStack, Image, Text } from "@chakra-ui/react";
 const WordCloud = () => {
   return (
-    <VStack w='80vw' pt='2em' h='100vh' ml={"20em"} spacing='2em'>
+    <VStack w='80vw' pt='2em' h='100vh' spacing='2em' overflow='scroll'>
+      <Text fontWeight='bold' fontSize='xl'>
+        word cloud for depression_stress_words
+      </Text>
       <Image
-        src='https://res.cloudinary.com/dlzjopwm0/image/upload/v1682013829/wordcloud_vlmvpg.ico'
-        boxSize={"40em"}
+        src='https://res.cloudinary.com/dlzjopwm0/image/upload/v1682902682/stress_words_word_cloud_cjdi5j.png'
+        boxSize={"30em"}
       />
+      <Text fontWeight='bold' fontSize='xl'>
+        word cloud for Highest Scores
+      </Text>
+      <Image
+        src='https://res.cloudinary.com/dlzjopwm0/image/upload/v1682903135/stress_score_cloud_v665ii.png'
+        boxSize={"30em"}
+      />
+      <Text fontWeight='bold' fontSize='xl'>
+        word cloud for medium stress score
+      </Text>
+      <Image
+        src='https://res.cloudinary.com/dlzjopwm0/image/upload/v1682902682/stress_score_word_cloud_jjtd8e.png'
+        boxSize={"30em"}
+      />
+      <Text></Text>
     </VStack>
   );
 };
